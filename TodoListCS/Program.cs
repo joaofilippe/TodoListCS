87a1036632444e8f5b03297de9d48067db0d5a88
joaof <joaof@FILIPPE-PC>
2023-09-03 17:39:57 -0300

    Adicionar arquivos de projeto.

---
+++ TodoListCS/Program.cs
@@ -0,0 +1,25 @@
+﻿void Welcome()
+{
+    string programName = @"
+░█████╗░██╗░░░░░██╗░██████╗████████╗
+██╔══██╗██║░░░░░██║██╔════╝╚══██╔══╝
+██║░░╚═╝██║░░░░░██║╚█████╗░░░░██║░░░
+██║░░██╗██║░░░░░██║░╚═══██╗░░░██║░░░
+╚█████╔╝███████╗██║██████╔╝░░░██║░░░
+░╚════╝░╚══════╝╚═╝╚═════╝░░░░╚═╝░░░";
+    string welcomeMessage = "Bem-vindo ao CLIST";
+
+    Console.WriteLine(programName);
+    Console.WriteLine();
+}
+
+void MenuOptions()
+{
+    Console.WriteLine("Digite 1 para cadastrar uma tarefa:");
+    Console.WriteLine("Digite 2 para mostrar todas as suas tarefas:");
+    Console.WriteLine("Digite 3 para concluir uma tarefa:");
+    Console.WriteLine("Digite 4 para excluir uma tarefa:");
+}
+
+Welcome();
+MenuOptions();
